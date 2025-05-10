95c84bc09ae94c1b091892e7d251b56a2033311c
Musa Baloyi <musa.baloyi@ibm.com>
2025-05-10 11:48:54 +0200

    Remove preprocessing steps; add test_card

--- prepare/cards/schema_linking.py
+++ prepare/cards/schema_linking.py
@@ -3,25 +3,18 @@ import json
 from unitxt.blocks import Set, TaskCard
 from unitxt.catalog import add_to_catalog
 from unitxt.loaders import LoadFromDictionary
+from unitxt.test_utils.card import test_card
 
 with open("linked_schema.json") as f:
     data = json.load(f)
 
 card = TaskCard(
     loader=LoadFromDictionary(data={"test": data}),
-    preprocess_steps=[
-        Set(
-            fields={
-                "hint": "",
-                "id": "",
-            }
-        ),
-    ],
     task="tasks.schema_linking",
     templates="templates.schema_linking.all",
 )
 
-# test_card(card)
+test_card(card, debug=False)
 
 add_to_catalog(
     artifact=card,
